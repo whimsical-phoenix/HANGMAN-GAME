e0f9bb1de5a7410892fbdef7696e88d2685aa0ca
whimsical-phoenix <shahprapti12@gmail.com>
2024-01-31 15:45:14 -0700

    removed background phone responsive

--- hangman-frontend/src/App.js
+++ hangman-frontend/src/App.js
@@ -2,12 +2,12 @@
 import React from "react";
 import "./App.css";
 import Game from "./components/Game";
-import BackgroundCanvas from "./components/BackgroundCanvas";
+// import BackgroundCanvas from "./components/BackgroundCanvas";
 
 function App() {
   return (
     <div className="App">
-      <BackgroundCanvas />
+      {/* <BackgroundCanvas /> */}
       <Game />
     </div>
   );
