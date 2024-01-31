677abfeeeb306c2569eca414e8884394217f90df
whimsical-phoenix <shahprapti12@gmail.com>
2024-01-31 14:54:18 -0700

    build scripts

--- server/server.js
+++ server/server.js
@@ -1,4 +1,3 @@
-
 //src/server/server.js
 
 require("dotenv").config(); // Load environment variables from .env file
@@ -7,10 +6,12 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const gameRouter = require("./routes/game");
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 10000; // Update to match Render configuration
 
+const pathToBuild = path.join(__dirname, "../hangman-frontend/build");
 app.use(cors());
 app.use(express.json());
 
@@ -23,6 +24,7 @@ connection.once("open", () => {
   console.log("MongoDB connection established successful");
 });
 
+app.use(express.static(pathToBuild));
 app.use("/api/game", gameRouter);
 
 app.listen(PORT, "0.0.0.0", () => {
